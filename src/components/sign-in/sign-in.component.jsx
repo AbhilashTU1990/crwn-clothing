a94ee9f1a7abfe09c5af1a449821bf7f1b0213e1
Abhilash T U <Abhilash.T.U@gds.ey.com>
2021-12-08 19:37:43 +0530

    Sign in with email and pwd

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import './sign-in.styles.scss';
-import { signInWithGoogle} from '../../firebase/firebase.utils';
+import { auth,signInWithGoogle} from '../../firebase/firebase.utils';
 class SignIn extends React.Component{
     constructor(props){
         super(props);
@@ -16,8 +16,16 @@ class SignIn extends React.Component{
 
     handleSubmit = event => {
         event.preventDefault();
+        
+        const {email, password} = this.state;
 
-        this.setState({email:'', password:''});
+        try{
+            auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email:'', password:''});
+        }catch (error){
+            console.log(error);
+        }
+        //this.setState({email:'', password:''});
     }
 
     handleChange = event =>{
